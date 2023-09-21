6ee927f59f00b9a4d2ef969c58d43369998368c6
akbariharshita <akbariharshita2002@gmail.com>
2023-09-21 10:28:45 +0530

    first commit

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -18,7 +18,7 @@ export default function Plans() {
                     </div>
                     <button className='bg-[#00df9a] w-52 rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
                 </div>
-                <div className='shadow-xl w-full flex flex-col p-4 my-4 mt-10 hover:scale-105 duration-[300ms]'>
+                <div className='shadow-xl bg-gray-100 w-full flex flex-col p-4 my-4 hover:scale-105 duration-[300ms]'>
                     <img src={Double} className='w-24 mx-auto mt-[-3rem] bg-white' alt='/' />
                     <h2 className='text-2xl font-bold text-center py-8'>Digital Marketing</h2>
                     <p className='text-center text-4xl font-bold'>$149</p>
@@ -29,7 +29,7 @@ export default function Plans() {
                     </div>
                     <button className='bg-[#00df9a] w-52 rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
                 </div>
-                <div className='shadow-xl w-full flex flex-col p-4 my-4 mt-10 hover:scale-105 duration-[300ms]'>
+                <div className='shadow-xl w-full flex flex-col p-4 my-4 hover:scale-105 duration-[300ms]'>
                     <img src={Triple} className='w-24 mx-auto mt-[-3rem] bg-white' alt='/' />
                     <h2 className='text-2xl font-bold text-center py-8'>App Development</h2>
                     <p className='text-center text-4xl font-bold'>$149</p>
